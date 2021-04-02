42a34b0130d235f0fec767374b2bf19af6de0195
artginzburg <art.ginzburg@ya.ru>
2021-04-02 15:39:06 +0300

    refactor Popup class

--- scripts/index.js
+++ scripts/index.js
@@ -14,29 +14,25 @@ class Popup {
     this._element = element;
 
     this._closeButton = this._element.querySelector('.popup__close-button');
-
-    // the methods below are set through constructor() because otherwise `this` called inside of a callback function points to `event.target`, and `.bind(this)` is bad for memory 'cause it creates a new function on every call
-    this.toggle = () => {
-      this._element.classList.contains(this._elementOpenedClass)
-        ? this._removeListeners()
-        : this._setListeners();
-
-      this._element.classList.toggle(this._elementOpenedClass);
-    };
-
-    this._clickHandler = e =>
-      (e.target === e.currentTarget || e.target === this._closeButton)
-        && this.toggle();
-
-    this._keypressHandler = e =>
-      (e.key === 'Escape' && !e.altKey && !e.ctrlKey && !e.metaKey && !e.shiftKey)
-        && this.toggle();
-    ;
   }
 
-  toggle() { this.toggle() } // necessary for `super.toggle()` to work since it's explicitly set through constructor()
-
   _elementOpenedClass = 'popup_opened';
+
+  toggle = () => {
+    this._element.classList.contains(this._elementOpenedClass)
+      ? this._removeListeners()
+      : this._setListeners();
+
+    this._element.classList.toggle(this._elementOpenedClass);
+  }
+
+  _clickHandler = e =>
+    (e.target === e.currentTarget || e.target === this._closeButton)
+      && this.toggle();
+
+  _keypressHandler = e =>
+    (e.key === 'Escape' && !e.altKey && !e.ctrlKey && !e.metaKey && !e.shiftKey)
+      && this.toggle();
 
   _setListeners() {
     this._element.addEventListener('click', this._clickHandler);
