a32b7b3d1b744f014863682f3eaba424f7f7c5eb
artginzburg <art.ginzburg@ya.ru>
2021-04-13 12:54:28 +0300

    create UserInfo class

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Card from '../components/Card.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import FormValidator from '../components/FormValidator.js';
 import Section from '../components/Section.js';
+import UserInfo from '../components/UserInfo.js';
 
 import {
   defaultFormConfig,
@@ -18,14 +19,18 @@ profileEditorValidator.enableValidation();
 const nameInput = profileEditor.form.elements.name;
 const jobInput = profileEditor.form.elements.job;
 
-const nameElement = document.querySelector('.profile__name');
-const jobElement = document.querySelector('.profile__description');
+const profileUserInfo = new UserInfo({
+  nameSelector: '.profile__name',
+  jobSelector: '.profile__description'
+});
 
 profileEditor.form.addEventListener('reset', e => {
   e.preventDefault();
 
-  nameInput.value = nameElement.textContent;
-  jobInput.value = jobElement.textContent;
+  const currentUserData = profileUserInfo.getUserInfo();
+
+  nameInput.value = currentUserData.name;
+  jobInput.value = currentUserData.job;
 });
 
 const profileEditorOpenButton = document.querySelector('.profile__edit-button');
@@ -36,8 +41,10 @@ profileEditorOpenButton.addEventListener('click', () => {
 });
 
 profileEditor.submitHandler = () => {
-  nameElement.textContent = nameInput.value;
-  jobElement.textContent = jobInput.value;
+  profileUserInfo.setUserInfo({
+    name: nameInput.value,
+    job: jobInput.value
+  });
 };
 
 // FEAT: Initial card loading
